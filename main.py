0fb7596db9a1efab3e670794e6b4d52a19ae9d84
hyeonjoonpark <pjjoon1379@gmail.com>
2022-12-27 11:44:23 +0900

    혁명 css 추가

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+local_css("style.css")
+
 @st.cache(allow_output_mutation=True)
 def cached_model():
     model = SentenceTransformer('jhgan/ko-sroberta-multitask')
@@ -31,11 +36,11 @@ with st.form('form', clear_on_submit=True):
 # 유저의 상태를 체크(세션 생성)
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
-# 유저의 상태를 채크
+# 유저의 상태를 채크;
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
-# 제출과 
+# 제출과 ;
 if submitted and user_input:
     embedding = model.encode(user_input)
 
@@ -43,27 +48,37 @@ if submitted and user_input:
     answer = df.loc[df['distance'].idxmax()]
 
     st.session_state.past.append(user_input)
-    if answer['distance'] > 0.5:
+    if answer['distance'] > 0.65
         st.session_state.generated.append(answer['챗봇'])
     else:
-        st.session_state.generated.append('히히 무슨 말인지 모름')
+        st.session_state.generated.append('자세한 상황은 051 - 971- 2153으로 연락 바랍니다.')
 
 for i in range(len(st.session_state['past'])):
-    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-    if len(st.session_state['generated']) > i:
-        message(st.session_state['generated'][i], key=str(i) + '_bot')
+    # message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    # if len(st.session_state['generated']) > i:
+    #     message(st.session_state['generated'][i], key=str(i) + '_bot')
 
+    st.markdown(
+        """
+            <div class="msg right-msg>
+                <div class="msg-img></div>;
+                <div class="msg-bubble">
+                    <div class="msg-info">
+                        <div class="msg info-time">12:45</div>
+                    </div>
+                    <p>{0}</p>
+                </div>
+            </div>
 
-
-# picture = st.camera_input("Take a picture")
-
-
-
-img_file_buffer = st.camera_input("Take a picture!")
-
-if img_file_buffer:
-    st.image(img_file_buffer)
-
-if img_file_buffer is not None:
-    bytes_data = img_file_buffer.getvalue()
-    # st.write(type(bytes_data))
+            <div class="msg left-msg">
+                <div class="msg-img"></div>
+                <div class="msg-img></div>
+                <div class="msg-bubble">
+                    <div class="msg-info">
+                        <div class="msg info-time">12:46</div>
+                    </div>
+                    <p>{1}</p>
+                </div>
+            </div>
+        """.format(st.session_state['past'][i], st.session_state['generated'][i])
+    , unsafe_allow_html=True)
